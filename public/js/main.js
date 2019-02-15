85e635a3bc0072288634154c2e5cbec323c30438
ChauDinh <chaudvb95uc@gmail.com>
2019-02-15 10:36:14 +0700

    Render PDF File

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,83 @@
 document.getElementById("wrapper").innerHTML = `
-	<header>
-		<h3>Day la thanh tieu de</h3>
+	<header class="top-bar">
+		<button class="btn" id="prev-page">
+			<i class="fas fa-arrow-circle-left"></i> Prev Page
+		</button>
+		<button class="btn" id="next-page">
+			Next Page <i class="fas fa-arrow-circle-right"></i>
+		</button>
+		<span class="page-info">
+			Page <span id="page-num"></span> of <span id="page-count"></span>
+		</span>
 	</header>
 	<main>
-		<h1>Day la phan hien thi noi dung file PDF</h1>
+		<canvas id="pdf-render"></canvas>	
 	</main>
 	<footer>
 		<h3>Day la footer</h3>
 	</footer>
 `;
+
+const url = "../../src/demo.pdf";
+
+let pdfDoc = null,
+	pageNum = 1,
+	pageIsRendering = false,
+	pageNumIsPending = null;
+
+const scale = 1.5,
+	canvas = document.querySelector("#pdf-render"),
+	ctx = canvas.getContext("2d");
+
+// Render the page
+const renderPage = num => {
+	pageIsRendering = true;
+
+	// Get the page
+	pdfDoc.getPage(num).then(page => {
+		// Set scale
+		const viewport = page.getViewport({ scale });
+		canvas.height = viewport.height;
+		canvas.width = viewport.width;
+
+		const renderCtx = {
+			canvasContext: ctx,
+			viewport
+		};
+
+		page.render(renderCtx).promise.then(() => {
+			pageIsRendering = false;
+
+			if (pageNumIsPending !== null) {
+				renderPage(pageNumIsPending);
+				pageNumIsPending = null;
+			}
+		});
+
+		// Output current page
+		document.querySelector("#page-num").textContent = num;
+	});
+};
+
+// Check for pages rendering
+const queueRenderPage = num => {
+	if (pageIsRendering) {
+		pageNumIsPending = num;
+	} else {
+		renderPage(num);
+	}
+};
+
+// Show previous page
+const showPrevPage = () => {
+	
+}
+
+// Get Document
+pdfjsLib.getDocument(url).promise.then(pdfDoc_ => {
+	pdfDoc = pdfDoc_;
+
+	document.querySelector("#page-count").textContent = pdfDoc.numPages;
+
+	renderPage(pageNum);
+});
